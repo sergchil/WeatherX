76da1f9c9947cf148e95a18c8a0f1b1f6814e885
Chilingaryan <>
2022-02-28 21:48:22 +0400

    add view binding

--- app/src/main/java/com/chilisoft/weatherx/MainActivity.kt
+++ app/src/main/java/com/chilisoft/weatherx/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.chilisoft.weatherx
 
 import android.os.Bundle
-import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
+import com.chilisoft.weatherx.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // make edge to edge
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        setContentView(R.layout.activity_main)
 
-        val mainContent = findViewById<ViewGroup>(R.id.main_content)
-        ViewCompat.setOnApplyWindowInsetsListener(mainContent) { view, windowInsets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleInsets()
+    }
+
+    private fun handleInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.mainContent) { view, windowInsets ->
             val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-
             view.updateLayoutParams<FrameLayout.LayoutParams> {
                 leftMargin = insets.left
                 bottomMargin = insets.bottom
